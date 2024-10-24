5a94ec6cdf69418545a8b858facb8ca908faf8a9
heijsilva <132012711+heijsilva@users.noreply.github.com>
2024-10-23 21:05:33 -0300

    Add files via upload

---
+++ exercicio2.js
@@ -0,0 +1,8 @@
+// exercicio2.js  
+function exercicio2() {  
+    const num1 = parseFloat(prompt("Digite o primeiro número:"));  
+    const num2 = parseFloat(prompt("Digite o segundo número:"));  
+    const num3 = parseFloat(prompt("Digite o terceiro número:"));  
+    const media = (num1 + num2 + num3) / 3;  
+    alert("A média é: " + media);  
+}
---
+++ exercicio3.js
@@ -0,0 +1,6 @@
+// exercicio3.js  
+function exercicio3() {  
+    const numero = parseInt(prompt("Digite um número:"));  
+    const resultado = (numero % 2 === 0) ? "par" : "ímpar";  
+    alert("O número é: " + resultado);  
+}
